7d645e0b02dfa7695e67316344967e25b0f53828
Incheol Jung <bluesky761@naver.com>
2020-11-18 22:56:49 +0900

    first commit

---
+++ src/main/java/com/selldok/toy/ToyApplication.java
@@ -0,0 +1,13 @@
+package com.selldok.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/selldok/toy/ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selldok.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
