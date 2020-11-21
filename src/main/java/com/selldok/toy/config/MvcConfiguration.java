6e7690d539b438133617b2d19ddef752fcdd5e79
Incheol Jung <bluesky761@naver.com>
2020-11-21 09:50:57 +0900

    view page 보기

---
+++ src/main/java/com/selldok/toy/config/MvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.selldok.toy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Incheol Jung
+ */
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("file:src/main/resources/templates/", "file:src/main/resources/static/");
+    }
+}
--- src/main/java/com/selldok/toy/employee/controller/EmployeeController.java
+++ src/main/java/com/selldok/toy/employee/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import com.selldok.toy.employee.service.EmployeeService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +15,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 /**
  * @author Incheol Jung
  */
-@RestController
+@Controller
 @RequestMapping("employees")
 public class EmployeeController {
 
@@ -29,24 +30,33 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeService employeeService) {this.employeeService = employeeService;}
 
+    @GetMapping("view")
+    public String getEmployeeView(){
+        return "employee.html";
+    }
+
     @GetMapping
+    @ResponseBody
     public ResponseEntity<List<Employee>> get() {
         return new ResponseEntity(employeeService.get(), HttpStatus.OK);
     }
 
     @PostMapping
+    @ResponseBody
     public ResponseEntity insert(@RequestBody InsertEmployeeRequest request) {
         employeeService.insert(request);
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
     @PutMapping("{id}")
+    @ResponseBody
     public ResponseEntity update(@PathVariable("id") Long id, @RequestBody UpdateEmployeeRequest request) {
         employeeService.update(id, request);
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("{id}")
+    @ResponseBody
     public ResponseEntity delete(@PathVariable("id") Long id) {
         employeeService.delete(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
