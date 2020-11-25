b057a93771f2e9a2fed8ef16f7f82693886fe0e2
Incheol Jung <bluesky761@naver.com>
2020-11-26 00:40:32 +0900

    entity 수정(getter, setter 추가)

--- src/main/java/com/selldok/toy/employee/entity/School.java
+++ src/main/java/com/selldok/toy/employee/entity/School.java
@@ -2,10 +2,15 @@ package com.selldok.toy.employee.entity;
 
 import javax.persistence.Embeddable;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Incheol Jung
  */
 @Embeddable
+@Getter
+@Setter
 public class School {
     private String schoolName;
     private String department;
