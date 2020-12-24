4f0f28657cf7de2fc0cd6b4081888e6a7137f7a8
dskim <bluedskim@gmail.com>
2020-12-25 00:33:52 +0900

    지원목록 querydsl 작업 중

---
+++ src/main/java/com/selldok/toy/employee/dao/ApplyHistoryRepositoryImpl.java
@@ -0,0 +1,29 @@
+package com.selldok.toy.employee.dao;
+
+import java.util.List;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import static com.selldok.toy.employee.entity.QApplyHistory.*;
+import com.selldok.toy.employee.model.ApplyHistoryDto;
+import com.selldok.toy.employee.model.QApplyHistoryDto;
+
+/**
+ * @author DongSeok,Kim
+ */
+public class ApplyHistoryRepositoryImpl implements ApplyHistoryRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
+    public ApplyHistoryRepositoryImpl(JPAQueryFactory queryFactory) {
+        this.queryFactory = queryFactory;
+    }
+
+    @Override
+    public List<ApplyHistoryDto> search(ApplyHistoryDto searchCondition) {
+			return queryFactory
+			.select(
+				new QApplyHistoryDto(applyHistory.applicant.info.name)
+			)
+			.from(applyHistory)
+			.fetch();
+    }
+}
