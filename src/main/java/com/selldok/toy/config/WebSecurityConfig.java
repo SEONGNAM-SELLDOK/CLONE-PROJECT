022501728d772f05b4fb538efd5dcead0b99052f
dskim <bluedskim@gmail.com>
2020-12-25 16:14:48 +0900

    test하기 위해 추가한 permitAll() 제거

--- src/main/java/com/selldok/toy/config/WebSecurityConfig.java
+++ src/main/java/com/selldok/toy/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .permitAll()
             .antMatchers("/board/**")
             .permitAll()
-            .antMatchers("/applyHistories/**")
+            .antMatchers("/employees/**")
             .permitAll()
             .antMatchers("/employees", HttpMethod.POST.toString())
             .permitAll()
