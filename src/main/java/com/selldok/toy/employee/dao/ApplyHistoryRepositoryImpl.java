f271dabd7c06a9a8daf2db29a7d27268822eb26c
dskim <bluedskim@gmail.com>
2020-12-26 10:43:58 +0900

    queryFactory 호출시 들여쓰기 수정

--- src/main/java/com/selldok/toy/employee/dao/ApplyHistoryRepositoryImpl.java
+++ src/main/java/com/selldok/toy/employee/dao/ApplyHistoryRepositoryImpl.java
@@ -21,27 +21,27 @@ public class ApplyHistoryRepositoryImpl implements ApplyHistoryRepositoryCustom 
 	@Override
 	public List<ApplyHistoryDto> search(ApplyHistoryDto searchCondition) {
 		return queryFactory
-		.select(
-			new QApplyHistoryDto(
-				applyHistory.basicInfo.name
-				,applyHistory.basicInfo.email
-				,applyHistory.basicInfo.phoneNumber
-				,applyHistory.employmentBoard.company.name
-				,applyHistory.employmentBoard.title
-				,applyHistory.appliedDt
-				,applyHistory.status
+			.select(
+				new QApplyHistoryDto(
+					applyHistory.basicInfo.name
+					,applyHistory.basicInfo.email
+					,applyHistory.basicInfo.phoneNumber
+					,applyHistory.employmentBoard.company.name
+					,applyHistory.employmentBoard.title
+					,applyHistory.appliedDt
+					,applyHistory.status
+				)
 			)
-		)
-		.from(applyHistory)
-		.where(
-			// where 코딩을 좀 더 깔끔하게 할 수 있는 방법이 없을까요?
-			searchCondition.getName() != null ? applyHistory.basicInfo.name.contains(searchCondition.getName()) : null
-			,searchCondition.getCompanyName() != null ? applyHistory.employmentBoard.company.name.contains(searchCondition.getCompanyName()) : null
-			,searchCondition.getApplicantId() != null ? applyHistory.applicant.id.eq(searchCondition.getApplicantId()) : null
-		)
-		.offset(searchCondition.getOffset())
-		.limit(searchCondition.getLimit())
-		.orderBy(applyHistory.id.desc())
-		.fetch();
+			.from(applyHistory)
+			.where(
+				// where 코딩을 좀 더 깔끔하게 할 수 있는 방법이 없을까요?
+				searchCondition.getName() != null ? applyHistory.basicInfo.name.contains(searchCondition.getName()) : null
+				,searchCondition.getCompanyName() != null ? applyHistory.employmentBoard.company.name.contains(searchCondition.getCompanyName()) : null
+				,searchCondition.getApplicantId() != null ? applyHistory.applicant.id.eq(searchCondition.getApplicantId()) : null
+			)
+			.offset(searchCondition.getOffset())
+			.limit(searchCondition.getLimit())
+			.orderBy(applyHistory.id.desc())
+			.fetch();
 	}
 }
