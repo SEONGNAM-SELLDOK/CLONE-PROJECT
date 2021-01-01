931ebd59a768897481da7116db002903c9ad36a7
dskim <bluedskim@gmail.com>
2021-01-01 15:35:45 +0900

    프로필 화면으로 링크 추가

--- src/main/java/com/selldok/toy/employee/dao/ApplyHistoryRepositoryImpl.java
+++ src/main/java/com/selldok/toy/employee/dao/ApplyHistoryRepositoryImpl.java
@@ -55,7 +55,7 @@ public class ApplyHistoryRepositoryImpl implements ApplyHistoryRepositoryCustom 
 	/**
 	 * 지원자명과 회사명이 둘 다 넘어오는 경우 or 검색을 수행
 	 * 
-	 * sql로 표현하자면 and (name like '%키워드' or companuName like '%키워드%') 인데
+	 * sql로 표현하자면 and (name like '%키워드%' or companuName like '%키워드%') 인데
 	 * 이것을 querydsl로 코딩하려 하니 많이 지저분해집니다. 좋은 방법이 있을까요?
 	 */
 	private BooleanExpression containsOr(String name, String companyName) {
