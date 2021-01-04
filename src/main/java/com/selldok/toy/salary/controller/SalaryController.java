36a7dd815e99ca6316895fe0d697e1a1f3b45e90
parksale <psi0613@naver.com>
2021-01-04 21:42:21 +0900

    salary 페이지 view

--- src/main/java/com/selldok/toy/salary/controller/SalaryController.java
+++ src/main/java/com/selldok/toy/salary/controller/SalaryController.java
@@ -1,12 +1,7 @@
 package com.selldok.toy.salary.controller;
 
-import com.selldok.toy.salary.entity.Occupation;
-import com.selldok.toy.salary.model.SalaryRequest;
-import com.selldok.toy.salary.model.SalaryResponse;
 import com.selldok.toy.salary.service.SalaryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -17,13 +12,8 @@ public class SalaryController {
     private final SalaryService salaryService;
 
     @GetMapping("/salary")
-    public ResponseEntity<SalaryResponse> getSalaryView(SalaryRequest salaryRequest){
-
-        if(salaryRequest.getOccupation() == null){
-            salaryRequest.setOccupation(Occupation.dotNet);
-        }
-        SalaryResponse salaryResponse = salaryService.searchSalary(salaryRequest);
-        return new ResponseEntity(salaryResponse, HttpStatus.OK);
+    public String getSalaryView(){
+        return "/salary/main";
     }
 
 }
