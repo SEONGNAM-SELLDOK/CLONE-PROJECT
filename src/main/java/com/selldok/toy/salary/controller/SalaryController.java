9732b8a19c1493027ba17aef33cdfc1f4d513227
parksale <psi0613@naver.com>
2021-01-05 23:10:44 +0900

    salary Update

--- src/main/java/com/selldok/toy/salary/controller/SalaryController.java
+++ src/main/java/com/selldok/toy/salary/controller/SalaryController.java
@@ -1,5 +1,6 @@
 package com.selldok.toy.salary.controller;
 
+import com.selldok.toy.salary.model.SalaryRequest;
 import com.selldok.toy.salary.model.SalaryResponse;
 import com.selldok.toy.salary.service.SalaryService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 /**
  * @author Seil Park
@@ -26,6 +28,12 @@ public class SalaryController {
     @GetMapping("/salary/{id}")
     public ResponseEntity<SalaryResponse> getSalary(@PathVariable Long id){
         SalaryResponse salaryResponse = salaryService.getSalaryById(id);
-        return new ResponseEntity<>(salaryResponse, HttpStatus.OK);
+        return new ResponseEntity<>(salaryResponse,HttpStatus.OK);
+    }
+
+    @PutMapping("/salary/{id}")
+    public String updateSalary(@PathVariable Long id, SalaryRequest salaryRequest){
+        salaryService.updateSalary(salaryRequest);
+        return "redirect:/salary/" + id;
     }
 }
--- src/main/java/com/selldok/toy/salary/service/SalaryService.java
+++ src/main/java/com/selldok/toy/salary/service/SalaryService.java
@@ -8,9 +8,8 @@ import com.selldok.toy.salary.model.SalaryRequest;
 import com.selldok.toy.salary.model.SalaryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 /**
@@ -30,9 +29,9 @@ public class SalaryService {
         Salary salary3 = new Salary(Occupation.embedded,new SalaryInfo(1500,2000,2500,3000,4500,5500,7500,8500,9000,11000));
         Salary salary4 = new Salary(Occupation.fe,new SalaryInfo(500,1000,1500,2500,3000,3500,5500,6000,7000,7500));
         salaryRepository.save(salary1); salaryRepository.save(salary2); salaryRepository.save(salary3); salaryRepository.save(salary4);
-        System.out.println("=======================미리 집어넣기=====================");
     }
 
+    @Transactional(readOnly = true)
     public SalaryResponse getSalaryById(Long id){
         if(id > salaryRepository.count()){
             id = 0L;
@@ -50,7 +49,7 @@ public class SalaryService {
         return salaryResponse;
     }
 
-    public void setSalaryById(SalaryRequest salaryRequest){
+    public void updateSalary(SalaryRequest salaryRequest){
         Optional<Salary> optionalSalary = salaryRepository.findByOccupation(salaryRequest.getOccupation());
 
         if(optionalSalary.get() == null){
