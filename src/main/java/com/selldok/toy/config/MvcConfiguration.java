d3979e9ec22d7854e12a4296a4bab2588808a499
Incheol Jung <incheol.jung@navercorp.com>
2021-01-06 23:16:19 +0900

    bug fix

--- src/main/java/com/selldok/toy/config/MvcConfiguration.java
+++ src/main/java/com/selldok/toy/config/MvcConfiguration.java
@@ -10,9 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("file:src/main/resources/templates/", "file:src/main/resources/static/");
-    }
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+			.addResourceLocations("file:src/main/resources/templates/", "file:src/main/resources/static/");
+
+		registry.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
