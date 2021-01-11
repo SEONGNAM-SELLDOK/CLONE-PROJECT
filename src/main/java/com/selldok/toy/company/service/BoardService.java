e48ec5cfc77f0efc1c9c0894f3e6025eacda0e60
Incheol Jung <incheol.jung@navercorp.com>
2021-01-11 22:03:21 +0900

    임포트 정리

--- src/main/java/com/selldok/toy/company/service/BoardService.java
+++ src/main/java/com/selldok/toy/company/service/BoardService.java
@@ -1,21 +1,22 @@
 package com.selldok.toy.company.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.selldok.toy.company.dao.BoardRepository;
 import com.selldok.toy.company.entity.Board;
 import com.selldok.toy.company.model.BoardReadResponse;
 import com.selldok.toy.company.model.BoardUpdateRequest;
 import com.selldok.toy.company.model.NewHireListResponse;
 import com.selldok.toy.company.model.RecommendThisWeekResponse;
+import com.selldok.toy.config.CelldokFileUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.util.List;
-import java.util.Optional;
 /**
  * @author Gogisung
  */
@@ -24,54 +25,56 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @PropertySource(value = "classpath:/option.properties")
 public class BoardService {
-    private final BoardRepository boardRepository;
+	private final BoardRepository boardRepository;
 
-    private final CelldokFileUtil celldokFileUtil;
+	private final CelldokFileUtil celldokFileUtil;
 
-    public List<BoardReadResponse> findBoard(Long id) {
-        return boardRepository.findByBoardInfo(id);
-    }
+	public List<BoardReadResponse> findBoard(Long id) {
+		return boardRepository.findByBoardInfo(id);
+	}
 
-    public List<NewHireListResponse> newHireByBoardInfo() {
-        return boardRepository.newHireByBoardInfo();
-    }
+	public List<NewHireListResponse> newHireByBoardInfo() {
+		return boardRepository.newHireByBoardInfo();
+	}
 
-    public List<RecommendThisWeekResponse> recommendThisWeek() { return boardRepository.recommendThisWeek(); }
+	public List<RecommendThisWeekResponse> recommendThisWeek() {
+		return boardRepository.recommendThisWeek();
+	}
 
-    public int boardCountPlus(Long id) {
-        return boardRepository.boardCountPlus(id);
-    }
-    /**
-     * 구직 정보 등록
-     * */
-    public Long create(Board board) {
-        boardRepository.save(board);
-        return board.getId();
-    }
+	public int boardCountPlus(Long id) {
+		return boardRepository.boardCountPlus(id);
+	}
 
-    /**
-     * 구직정보수정
-     * */
-    public Long update(Long id, BoardUpdateRequest request) {
-        Optional<Board> board = boardRepository.findById(id);
+	/**
+	 * 구직 정보 등록
+	 * */
+	public Long create(Board board) {
+		boardRepository.save(board);
+		return board.getId();
+	}
 
-        board.ifPresent(existingCompany -> {
-            existingCompany.setTitle(request.getTitle());
-            existingCompany.setContent(request.getContent());
-            existingCompany.setImage(request.getImage());
-            existingCompany.setEndDate(request.getEndDate());
-            boardRepository.save(existingCompany);
-        });
+	/**
+	 * 구직정보수정
+	 * */
+	public Long update(Long id, BoardUpdateRequest request) {
+		Optional<Board> board = boardRepository.findById(id);
 
-        return id;
-    }
+		board.ifPresent(existingCompany -> {
+			existingCompany.setTitle(request.getTitle());
+			existingCompany.setContent(request.getContent());
+			existingCompany.setImage(request.getImage());
+			existingCompany.setEndDate(request.getEndDate());
+			boardRepository.save(existingCompany);
+		});
 
-    /**
-     * 파일 업로드
-     * */
-    public String saveUploadFile(MultipartFile upload_file) {
-        return celldokFileUtil.upload(upload_file);
-    }
+		return id;
+	}
 
+	/**
+	 * 파일 업로드
+	 * */
+	public String saveUploadFile(MultipartFile upload_file) {
+		return celldokFileUtil.upload(upload_file);
+	}
 
 }
