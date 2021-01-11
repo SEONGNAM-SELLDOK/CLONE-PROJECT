28d4c9bd22e384482d848a0c2ac348c7e5d7df40
Incheol Jung <bluesky761@naver.com>
2021-01-11 22:03:37 +0900

    이미지 업로드 기능 개선 (#64)
    * update upload image
    
    * remove resourceHandler
    
    * remove resourceHandler
    
    * 피알 리뷰 반영
    
    * 피알 리뷰 반영
    
    * 임포트 정리

--- src/main/java/com/selldok/toy/event/controller/EventController.java
+++ src/main/java/com/selldok/toy/event/controller/EventController.java
@@ -1,12 +1,12 @@
 package com.selldok.toy.event.controller;
 
-import com.selldok.toy.event.entity.Event;
-import com.selldok.toy.event.model.EventSearchRequest;
-import com.selldok.toy.event.model.InsertEventRequest;
-import com.selldok.toy.event.model.UpdateEventRequest;
-import com.selldok.toy.event.service.EventService;
+import java.security.Principal;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +17,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.security.Principal;
-import java.util.List;
-import java.util.Optional;
+import com.selldok.toy.event.entity.Event;
+import com.selldok.toy.event.model.EventSearchRequest;
+import com.selldok.toy.event.model.InsertEventRequest;
+import com.selldok.toy.event.model.UpdateEventRequest;
+import com.selldok.toy.event.service.EventService;
 
 /**
  * @author HJ Lee
@@ -30,57 +34,59 @@ import java.util.Optional;
 @RequestMapping("event")
 public class EventController {
 
-    private final EventService eventService;
+	private final EventService eventService;
 
-    public EventController(EventService eventService) {
-        this.eventService = eventService;
-    }
+	public EventController(EventService eventService) {
+		this.eventService = eventService;
+	}
 
-    @GetMapping("edit/{id}")
-    public String editPage(@PathVariable("id") Long id, Model model) {
-        Optional<Event> eventOptional = eventService.findById(id);
-        model.addAttribute("event", eventOptional.orElse(null));
-        return "event/eventedit";
-    }
+	@GetMapping("edit/{id}")
+	public String editPage(@PathVariable("id") Long id, Model model) {
+		Optional<Event> eventOptional = eventService.findById(id);
+		model.addAttribute("event", eventOptional.orElse(null));
+		return "event/eventedit";
+	}
 
-    @GetMapping("list")
-    public String listPage(Model model, Principal principal, EventSearchRequest request) {
-        request.setOwner(principal.getName());
-        model.addAttribute("events", eventService.getList(request));
-        return "event/eventlist";
-    }
+	@GetMapping("list")
+	public String listPage(Model model, Principal principal, EventSearchRequest request) {
+		request.setOwner(principal.getName());
+		model.addAttribute("events", eventService.getList(request));
+		return "event/eventlist";
+	}
 
-    @GetMapping("search")
-    public String searchPage(Model model, EventSearchRequest request) {
-        model.addAttribute("events", eventService.getList(request));
-        return "event/eventlistForjobseeker";
-    }
+	@GetMapping("search")
+	public String searchPage(Model model, EventSearchRequest request) {
+		model.addAttribute("events", eventService.getList(request));
+		return "event/eventlistForjobseeker";
+	}
 
-    @GetMapping("detail/{id}")
-    public String detailPage(@PathVariable("id") Long id, Model model) {
-        Optional<Event> eventOptional = eventService.findById(id);
-        model.addAttribute("event", eventOptional.orElse(null));
-        return "event/eventdetail";
-    }
+	@GetMapping("detail/{id}")
+	public String detailPage(@PathVariable("id") Long id, Model model) {
+		Optional<Event> eventOptional = eventService.findById(id);
+		model.addAttribute("event", eventOptional.orElse(null));
+		return "event/eventdetail";
+	}
 
-    @PostMapping
-    @ResponseBody
-    public ResponseEntity insert(@RequestBody InsertEventRequest request) {
-        eventService.insert(request);
-        return new ResponseEntity(HttpStatus.ACCEPTED);
-    }
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, headers = ("Content-Type=application/x-www-form-urlencoded"))
+	@ResponseBody
+	public ResponseEntity insert(HttpServletRequest servletRequest,
+		@RequestParam(value = "image", required = false) MultipartFile file,
+		InsertEventRequest request) {
+		eventService.insert(file, request);
+		return new ResponseEntity(HttpStatus.ACCEPTED);
+	}
 
-    @PutMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity update(@PathVariable("id") Long id, @RequestBody UpdateEventRequest request) {
-        eventService.update(id, request);
-        return new ResponseEntity(HttpStatus.ACCEPTED);
-    }
+	@PutMapping("/{id}")
+	@ResponseBody
+	public ResponseEntity update(@PathVariable("id") Long id, @RequestBody UpdateEventRequest request) {
+		eventService.update(id, request);
+		return new ResponseEntity(HttpStatus.ACCEPTED);
+	}
 
-    @DeleteMapping("/{id}")
-    @ResponseBody
-    public ResponseEntity delete(@PathVariable("id") Long id) {
-        eventService.delete(id);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
+	@DeleteMapping("/{id}")
+	@ResponseBody
+	public ResponseEntity delete(@PathVariable("id") Long id) {
+		eventService.delete(id);
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
 }
--- src/main/java/com/selldok/toy/event/service/EventService.java
+++ src/main/java/com/selldok/toy/event/service/EventService.java
@@ -1,85 +1,95 @@
 package com.selldok.toy.event.service;
 
-import com.selldok.toy.event.entity.Event;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.selldok.toy.config.CelldokFileUtil;
 import com.selldok.toy.event.dao.EventRepository;
+import com.selldok.toy.event.entity.Event;
 import com.selldok.toy.event.mapper.EventMapper;
 import com.selldok.toy.event.model.EventSearchRequest;
 import com.selldok.toy.event.model.InsertEventRequest;
 import com.selldok.toy.event.model.UpdateEventRequest;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
 /**
  * @author HJ Lee
  */
 @Service
+@Transactional
 public class EventService {
-    private final EventRepository eventRepository;
+	private final EventRepository eventRepository;
 
-    private final EventMapper eventMapper;
+	private final EventMapper eventMapper;
 
-    public EventService(EventRepository eventRepository, EventMapper eventMapper) {
-        this.eventRepository = eventRepository;
-        this.eventMapper = eventMapper;
-    }
+	private final CelldokFileUtil celldokFileUtil;
 
-    public Optional<Event> findById(Long id){
-        return eventRepository.findById(id);
-    }
+	public EventService(EventRepository eventRepository, EventMapper eventMapper,
+		CelldokFileUtil celldokFileUtil) {
+		this.eventRepository = eventRepository;
+		this.eventMapper = eventMapper;
+		this.celldokFileUtil = celldokFileUtil;
+	}
 
-    public List<Event> getList(EventSearchRequest request) {
-        return eventMapper.getEvent(request);
-    }
+	public Optional<Event> findById(Long id) {
+		return eventRepository.findById(id);
+	}
 
-    public void insert(InsertEventRequest request) {
-        Event event = Event.builder()
-                .imageLink(request.getImageLink())
-                .type(request.getType())
-                .title(request.getTitle())
-                .text(request.getText())
-                .date(request.getDate())
-                .keywords(request.getKeywords())
-                .owner(request.getOwner())
-                .location(request.getLocation())
-                .isFree(request.getIsFree())
-                .isRecommend(request.getIsRecommend())
-                .build();
+	public List<Event> getList(EventSearchRequest request) {
+		return eventMapper.getEvent(request);
+	}
 
-        eventRepository.save(event);
-    }
+	public void insert(MultipartFile file,
+		InsertEventRequest request) {
+		String imageUrl = celldokFileUtil.upload(file);
+		Event event = Event.builder()
+			.imageLink(imageUrl)
+			.type(request.getType())
+			.title(request.getTitle())
+			.text(request.getText())
+			.date(request.getDate())
+			.keywords(request.getKeywords())
+			.owner(request.getOwner())
+			.location(request.getLocation())
+			.isFree(request.getIsFree())
+			.isRecommend(request.getIsRecommend())
+			.build();
 
-    public void update(Long id, UpdateEventRequest request) {
-        Optional<Event> event = eventRepository.findById(id);
-        event.ifPresent(presentEvent -> {
-            presentEvent = Event.builder()
-                    .id(id)
-                    .imageLink(request.getImageLink())
-                    .type(request.getType())
-                    .title(request.getTitle())
-                    .text(request.getText())
-                    .date(request.getDate())
-                    .keywords(request.getKeywords())
-                    .owner(request.getOwner())
-                    .location(request.getLocation())
-                    .isFree(request.getIsFree())
-                    .isRecommend(request.getIsRecommend())
-                    .build();
+		eventRepository.save(event);
+	}
 
-            eventRepository.save(presentEvent);
-        });
-    }
+	public void update(Long id, UpdateEventRequest request) {
+		Optional<Event> event = eventRepository.findById(id);
+		event.ifPresent(presentEvent -> {
+			presentEvent = Event.builder()
+				.id(id)
+				.imageLink(request.getImageLink())
+				.type(request.getType())
+				.title(request.getTitle())
+				.text(request.getText())
+				.date(request.getDate())
+				.keywords(request.getKeywords())
+				.owner(request.getOwner())
+				.location(request.getLocation())
+				.isFree(request.getIsFree())
+				.isRecommend(request.getIsRecommend())
+				.build();
 
-    public void delete(Long id) {
-        eventRepository.deleteById(id);
-    }
+			eventRepository.save(presentEvent);
+		});
+	}
 
-    public List<Event> getLatest(int size) {
-        PageRequest pageRequest = PageRequest.of(0,size);
-        return eventRepository.findAllByOrderByDateDesc(pageRequest);
-    }
+	public void delete(Long id) {
+		eventRepository.deleteById(id);
+	}
+
+	public List<Event> getLatest(int size) {
+		PageRequest pageRequest = PageRequest.of(0, size);
+		return eventRepository.findAllByOrderByDateDesc(pageRequest);
+	}
 }
