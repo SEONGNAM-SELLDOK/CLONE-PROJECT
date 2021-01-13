9f0c85257280275f2ee6cfd23399ccbbe9297fe7
dskim <bluedskim@gmail.com>
2021-01-13 22:58:25 +0900

    데모 메소드 제거

--- src/main/java/com/selldok/toy/exception/RestApiExceptionHandler.java
+++ src/main/java/com/selldok/toy/exception/RestApiExceptionHandler.java
@@ -1,16 +1,10 @@
 package com.selldok.toy.exception;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
  * 모든 컨트롤러에서 RestApiException 발생시 처리할 핸들러
  */
 @Slf4j
-@RestController
 @RestControllerAdvice
 public class RestApiExceptionHandler {
 	/**
@@ -49,33 +42,5 @@ public class RestApiExceptionHandler {
 		log.error("handleException in {}", this.getClass().getSimpleName(), ex);
 		ApiErrorMsg apiErrorMsg = new ApiErrorMsg("정의되지 않은 오류가 발생하였습니다.", "N/A", HttpStatus.INTERNAL_SERVER_ERROR);
 		return new ResponseEntity<>(apiErrorMsg, apiErrorMsg.getStatus());
-	}	
-
-	/**
-	 * 데모용 
-	 * 
-	 * 정상 처리 테스트 : ~/throwExceptionDemo?throwException=true 
-	 * 오류 발생 테스트 : ~/throwExceptionDemo?throwException=false
-	 * 
-	 * <ApiErrorMsg 샘플> ApiErrorMsg apiErrorMsg = new ApiErrorMsg("호출자에게 알려줄 상세한 오류
-	 * 메시지"); ApiErrorMsg apiErrorMsg = new ApiErrorMsg("호출자에게 알려줄 상세한 오류 메시지",
-	 * "ERR001(해당 시스템에서 정의한 오류 코드)");
-	 * 
-	 * @param throwException
-	 * @return
-	 */
-	@GetMapping(value = "/throwExceptionDemo")
-	public Map<String, Object> throwExceptionTest(@RequestParam(required = false) Boolean throwException) {
-		log.error("throwException={}", throwException);
-
-		Map<String, Object> rtn = null;
-		if(throwException != null && throwException) {
-			throw new RestApiException(new ApiErrorMsg("호출자에게 알려줄 상세한 오류 메시지", "ERR001(해당 시스템에서 정의한 오류 코드)", HttpStatus.INTERNAL_SERVER_ERROR), new RuntimeException());
-		} else {
-			rtn = new HashMap<>();
-			rtn.put("successMsg", "정상 처리된 경우");
-		}
-		
-		return rtn;
 	}	
 }
