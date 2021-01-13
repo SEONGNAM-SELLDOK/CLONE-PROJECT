b7b53a4ce78cecbf097bdf67bc382c06bca9397b
dskim <bluedskim@gmail.com>
2021-01-13 12:57:41 +0900

    ApiErrorMsg만 받는 생성자 추가

--- src/main/java/com/selldok/toy/exception/RestApiException.java
+++ src/main/java/com/selldok/toy/exception/RestApiException.java
@@ -10,8 +10,24 @@ public class RestApiException extends RuntimeException {
 	 */
 	final transient ApiErrorMsg apiErrorMsg;
 
-	public RestApiException(ApiErrorMsg apiErrorMsg, Throwable err) {
-		super(err);
+	/**
+	 * ApiErrorMsg만 받는 생성자 
+	 * 
+	 * @param apiErrorMsg
+	 */
+	public RestApiException(ApiErrorMsg apiErrorMsg) {
+		super(apiErrorMsg.getMessage());
+		this.apiErrorMsg = apiErrorMsg;
+	}	
+
+	/**
+	 * ApiErrorMsg과 Throwable를 받는 생성자 
+	 * 
+	 * @param apiErrorMsg
+	 * @param Throwable cause 오류를 발생시킨 원인 Throwable을 넘겨준다.
+	 */
+	public RestApiException(ApiErrorMsg apiErrorMsg, Throwable cause) {
+		super(apiErrorMsg.getMessage(), cause);
 		this.apiErrorMsg = apiErrorMsg;
 	}
 
