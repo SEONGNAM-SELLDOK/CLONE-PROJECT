9559914cb9d74d5766cbf48a6c612cc26ffbe0c8
dskim <bluedskim@gmail.com>
2021-01-16 21:51:36 +0900

    들여쓰기 수정

--- src/main/java/com/selldok/toy/exception/CommonErrorCode.java
+++ src/main/java/com/selldok/toy/exception/CommonErrorCode.java
@@ -6,8 +6,8 @@ import com.selldok.toy.exception.RestApiException.ApiErrorCode;
  * 공통 api 에러 코드는 여기에 추가해 주세요
  */
 public enum CommonErrorCode implements ApiErrorCode {
-    /** 정의되지 않은 오류가 발생하였습니다. */
-    CMM_001("정의되지 않은 오류가 발생하였습니다.");
+	/** 정의되지 않은 오류가 발생하였습니다. */
+	CMM_001("정의되지 않은 오류가 발생하였습니다.");
 
 	private String errorMessage;
 
